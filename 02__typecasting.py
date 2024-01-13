006fcb1d554a82211ce32c5e7af8a7ee857dda59
tejhande <59686002+tejhande@users.noreply.github.com>
2024-01-13 17:01:37 +0530

    2_Typecasting.py
    Added Comments improved Code

--- 02__typecasting.py
+++
@@ -1,3 +0,0 @@
-i= 0
-for i in range(1, 1001):
-    print(i, "ILU")
---
+++ 2_Typecasting.py
@@ -0,0 +1,10 @@
+#Using Typecasting 
+
+i = int(10)
+print(i, type(i))   #10 <class 'int'>
+print(i+i)          #20
+
+i = str(i)
+print(i, type(i))   #10 <class 'str'>
+print(i+i)          #1010
+
