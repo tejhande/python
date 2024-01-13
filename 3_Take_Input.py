cf3deef19b097c9b7346dc240b67704ff8cd89b7
tejhande <59686002+tejhande@users.noreply.github.com>
2024-01-13 17:05:04 +0530

    3_Take_Input.py
    Added Comment And Improved Code

---
+++ 3_Take_Input.py
@@ -0,0 +1,10 @@
+# Take Input From User
+a = input("Enter Your Name: ")
+b = input("Enter Your Mobile Number: ")
+c = input("Enter Your Email Address: ")
+d = input("Enter Your Password: ")
+
+print(a)
+print(b)
+print(c)
+print(d)    
