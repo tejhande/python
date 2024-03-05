a3b667c0ad797b165d7052b60b77b354a1211424
Tejas Amol Hande <59686002+tejhande@users.noreply.github.com>
2024-03-05 17:41:36 +0530

    .

--- Addition.py
+++ Addition.py
@@ -1,9 +1,11 @@
-try:
+while True:
+    try:
 
-    a = input("Enter First Number: \n\t")
-    b = input("Enter Second Number: \n\t")
-    a = int(a)
-    b = int(b)
-    print("Addition Of", a , "And" , b , "Is:" ,a + b)
-except Exception as e:
-    print(e) #1
+        a = input("Enter First Number: \n\t")
+        b = input("Enter Second Number: \n\t")
+        a = int(a)
+        b = int(b)
+        print("Addition Of", a , "And" , b , "Is:" ,a + b)
+    except Exception as e:
+        print(e) #1
+        break
