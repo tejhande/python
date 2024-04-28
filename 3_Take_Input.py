64eb93825a933c37e834d61b286276ba982abd89
Tejas Amol Hande <59686002+tejhande@users.noreply.github.com>
2024-04-28 22:04:05 +0530

    Update 3_Take_Input.py

--- 3_Take_Input.py
+++ 3_Take_Input.py
@@ -2,7 +2,7 @@
 a = input("Enter Your Name: ")
 b = input("Enter Your Mobile Number: ")
 c = input("Enter Your Email Address: ")
-d = input("Enter Your Password: ")
+d = input("Enter Your Password : ")
 
 print(a)
 print(b)
